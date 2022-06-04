287b0db953048098486181ff1f6e0724ed3da5e0
mate1978 <mate.szabolcs2@gmail.com>
2022-06-05 00:07:10 +0200

    commit

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -2,7 +2,7 @@
 @section("content")
     <div class="custom-product">
         <div class="col-sm-4">
-            <a href="#">Szűrés</a>
+            <a href="/">Vissza</a>
         </div>
         <div class="col-sm-4">
             <div class="trending-wrapper">
